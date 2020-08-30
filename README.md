d9ccd55ff15e8ff1e559c26e72919f48619be112
Joseph-D98 <defelicejoseph@outlook.com>
2020-08-30 15:33:16 -0600

    local storage functionality/ readme update

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # work-day-scheduler
+
+A time managment tool to schedule a work day by hour. The current date and time displayed in the header are dynamically upated using Moment.js. Each row is also dynamically updated to change color based on the time of day based on the following key:
+
+Gray: Past Hours
+Red: Current Hour
+Green: Furture Hours
+
+Deployed URL: https://joseph-d98.github.io/work-day-scheduler/
+
+![](assets/image/readme.PNG)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
-// Variables
+// variables
 var saveBtn = $(".saveBtn");
 var currentHour = moment().format("HH"); // Variable for current hour 
 var currentHourInt = parseInt(currentHour); // Parse it so that hour returns as an integer
 
-// Set data attributes to each hour input element so that the function below can assign a color to each row based on the current hour
+// add data attributes for each hour to color to each row for current time
 $("#9Row").attr("data-time", moment("9:00 am", "h:mm a").format("HH"));
 $("#10Row").attr("data-time", moment("10:00 am", "hh:mm a").format("HH"));
 $("#11Row").attr("data-time", moment("11:00 am", "hh:mm a").format("HH"));
@@ -14,32 +14,49 @@ $("#3Row").attr("data-time", moment("3:00 pm", "h:mm a").format("HH"));
 $("#4Row").attr("data-time", moment("4:00 pm", "h:mm a").format("HH"));
 $("#5Row").attr("data-time", moment("5:00 pm", "h:mm a").format("HH"));
 
-//start jQuery 
+// start jQuery 
 $(document).ready(function () {
 
-    // Show Date and Time in Header 
+    // function to set data in local storage 
+    renderPlans();
+
+    // add date and time to header 
     $('#currentDay').append();
 
     function addDate() {
         $('#currentDay').html(moment().format('MMMM Do YYYY, h:mm a'));
+
     } setInterval(addDate, 1000);
 
-});
+    // change row color to reflect the current hour 
+    for (var i = 0; i <= 12; i++) {  
 
-
-// Change color in each row by adding in am attribute (above) for each input line to reflect the current hour 
-for (var i = 0; i <= 12; i++) {  
-
-    var inputHour = $("#" + i + "Row").attr("data-time"); // Variable for the hour of the row 
-    var inputHourInt = parseInt(inputHour); // Parse it so that hour returns as an integer
+    var inputHour = $("#" + i + "Row").attr("data-time"); // row hour variable
+    var inputHourInt = parseInt(inputHour); // parse hour to return integer
 
     if (currentHourInt === inputHourInt) {
-      $("#" + i + "Row").addClass("present"); // Applies red color if within the present hour 
+      $("#" + i + "Row").addClass("present"); // applies red color if within the present hour 
     }
-    if (currentHourInt > inputHourInt) { // Applies grey color if hour is in the future 
+    if (currentHourInt > inputHourInt) { // applies grey color if hour is in the past
       $("#" + i + "Row").addClass("past");
     }
-    if (currentHourInt < inputHourInt) { // Applies green color if hour is in the future 
+    if (currentHourInt < inputHourInt) { // applies green color if hour is in the future 
       $("#" + i + "Row").addClass("future");
     }  
-};
+  }
+
+    // on click function to set data in local storage
+    saveBtn.on("click", function () {
+
+        var rowHour = $(this).attr("data-hour"); // row hour variable
+        var input = $("#" + rowHour + "Row").val(); // saves text input
+        localStorage.setItem(rowHour, input); //sets in local storaage
+    });
+
+    //  function to get data from local storage 
+    function renderPlans() {
+        for (var i = 0; i <= 12; i++) {
+            $("#" + i + "Row").val(localStorage.getItem(i));
+        }
+    }
+});
